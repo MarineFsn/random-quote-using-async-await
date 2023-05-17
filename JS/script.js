fee5a9031b13a40cef00c54d152236a1b5cbd279
fassma89@gmail.com <fassma89@gmail.com>
2023-05-17 11:22:04 +0200

    JS in progress

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+//Définition des éléments:
+
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
+const newQuoteButton = document.getElementById("new-quote-btn");
+
+//Fonction Async, envoyer une requete à l'API et récupérer les citations:
+
+async function fetchQuote() {
+  return fetch("https://thatsthespir.it/api")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error fetching quote" + response.status);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("Error fetching quote:", error);
+      throw error;
+    });
+}
+
+//Fonction Async, générer une nouvelle citation:
+async function generateQuote() {
+  fetchQuote()
+    .then((quoteData) => {
+      displayQuote(quoteData);
+
+    //console.log(quoteData);
+    })
+    .catch((error) => {
+      quoteElement.textContent = "Failed to fetch quote, Please try again!";
+      authorElement.textContent = "";
+    });
+    
+}
+
+//Display la citation:
+function displayQuote(quoteData) {
+  quoteElement.textContent = quoteData.quote;
+  authorElement.textContent = quoteData.author;
+  document.getElementById("quote-image").src = quoteData.photo;
+}
+
+//Ecouteur d'évenement:
+newQuoteButton.addEventListener("click", generateQuote);
